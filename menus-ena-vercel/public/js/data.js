17d76d1c1c884c63d3eb920dd96118fef334ea16
Anato221 <122191472+Bixen221@users.noreply.github.com>
2026-05-03 18:35:17 +0000

    Refactor PLATS_DATA structure for meal categories

--- menus-ena-vercel/public/js/data.js
+++ menus-ena-vercel/public/js/data.js
@@ -1,38 +1,10 @@
 /**
  * DONNÉES DES PLATS - ENA Menu Collector
- * Organisé par catégorie, basé sur la transcription officielle des plats sénégalais.
- * Pour modifier les plats, éditez cet objet directement.
+ * Une seule catégorie : Repas de midi
  */
 
 const PLATS_DATA = {
-  "🍲 Bouillies": [
-    "Fondé bassi (Bouillie de granulés de sorgho)",
-    "Fondé dugub (Bouillie de granulés de mil)",
-    "Fondé mbokh (Bouillie de granulés de maïs)",
-    "Laaxu daxàr (Lakhou dakhar)",
-    "Laaxu nèëri (Lakhou neuteuri)",
-    "Laaxu soow (Lakhou sow)",
-    "Mouhamsa soow (Mouhamsa au lait)",
-    "Ruy (Bouillie de farine de mil)",
-    "Sombi Céeb / Goci (Bouillie de riz au lait caillé)",
-    "Sombi dugub (Bouillie de semoule de mil)",
-    "Sombi mbokh (Bouillie de maïs)",
-    "Sombi vermicelle (Bouillie de vermicelle)",
-    "Sombi / Thiuraye gërté (Sombi guerté)"
-  ],
-  "🥔 Tubercules": [
-    "Athiéké à la sénégalaise",
-    "Fufu (Foufou)",
-    "Rôti ñambi (Ragoût manioc)"
-  ],
-  "🥦 Légumineuses & Légumes": [
-    "Lentilles yapp (Lentilles à la viande)",
-    "Ndambé (Sauce niébé)",
-    "Petits pois ganar (Petits pois poulet)",
-    "Salade ordinaire",
-    "Salatu ñebbe (Salade de niébé)"
-  ],
-  "🍚 Riz": [
+  "Repas de midi": [
     "C'bon",
     "Céebu ganar bu wéex (Riz gras poulet)",
     "Céebu géj (Thiebou guedj)",
@@ -56,70 +28,18 @@ const PLATS_DATA = {
     "Daxin yapp (Dakhine viande)",
     "Domoda jën (Domoda poisson)",
     "Domoda yapp (Domoda viande)",
-    "Étodié",
-    "Futi (Fouty)",
     "Kaldu (Ragoût de poisson)",
-    "Maafe folléré (Mafé à l'oseille)",
-    "Maafe jën (Mafé poisson)",
     "Maafe yapp (Mafé viande)",
     "Mbaxal jën bu weex (Mbakhal poisson)",
     "Mbaxalu saalum (Mbakhal Saloum)",
     "Mbaxalu yapp (Mbakhal viande)",
     "Mboroxe (Sauce feuilles de manioc)",
     "Paella à la sénégalaise",
-    "Plassas",
-    "Riteufou / Dourang",
     "Sulluxu (Souloukhou)",
     "Suppu kanja (Sauce gombo)",
     "Yassa ganar (Yassa poulet)",
     "Yassa jën (Yassa poisson)",
     "Yaasa yapp (Yassa viande)"
-  ],
-  "🌾 Mil / Maïs / Fonio": [
-    "Caakri dugub (Thiakry mil)",
-    "Céeré baase yapp (Thiéré bassé viande)",
-    "Céeré basi baase keccax (Thiéré bassé kéthiakh)",
-    "Céeré mbuum (Thiéré mboum)",
-    "Céeré méew (Thiéré au lait)",
-    "Céeré ñebbe (Thiéré niébé)",
-    "Céeré tallalé mbok jën (Thiéré maïs poisson)",
-    "Céeré tam-xarit (Thiéré bomb)",
-    "Céeré yapp (Thiéré viande)",
-    "Couscous marocain viande à la sénégalaise",
-    "Gar",
-    "Laaxu caxàan / Ngurbàn (Lakhou thiakhane)",
-    "Maafe fonio (Mafé fonio)",
-    "Nélen (Niéleng)",
-    "Nélen muud (Niéleng moud)",
-    "Ngallàx (Ngalakh)",
-    "Ñiiri bunaa (Gniri bouna)"
-  ],
-  "🍜 Soupes & Sauces": [
-    "Fiirir (Poisson frit)",
-    "Ngeubeu / Pépéssou (Soupe de poisson)",
-    "Œuf surprise",
-    "Poisson farcis à la Saint-Louisienne",
-    "Poulet braisé",
-    "Poulet sauté",
-    "Sauce crevette",
-    "Sup yapp (Soupe de viande)",
-    "Touffé ganar",
-    "Touffé yapp"
-  ],
-  "🍝 Pâtes": [
-    "Maccaroni yapp (Maccaroni viande)",
-    "Spaghetti bolognaise",
-    "Spaghetti œufs",
-    "Spaghetti viande",
-    "Vermicelle poulet",
-    "Vermicelle viande"
-  ],
-  "🥟 Beignets & Snacks": [
-    "Akara (Beignets de niébé)",
-    "Béñee coopati (Beignets de farine)",
-    "Béñee dugub (Beignets de farine de mil)",
-    "Fataya yapp (Fataya viande)",
-    "Pastelles"
   ]
 };
 
